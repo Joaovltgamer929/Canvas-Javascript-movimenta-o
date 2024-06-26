0c586ad17de90514a2e08ca9bc7bf5b6376c7d63
João Vitor Lonardoni <134065160+Joaovltgamer929@users.noreply.github.com>
2024-06-26 15:44:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-(function(){
+(function(){    
     //Variaveis
     var cnv = document.querySelector("canvas");
-    var ctx = cnv.getContext("2d")
+    var ctx = cnv.getContext("2d");
     //Teclas
     var LEFT = 37;
     var UP = 38;
     var RIGTH = 39;
     var DOWN = 40;
-    //Movimentos
+    //Movimentos 
+    var mvLeft = mvUp = mvRigth = mvDown = false;
+    //Arrays
+    var sprites = [];
+    var blocks = []; 
+    //Objetos
+    var character = new Sprites(50, 175, 50, 50, "#00f");
+    sprites.push(character);
 
-}
+    //funcão que atualiza a po jogo
+    function loop(){
+        window.requestAnimationFrame(loop, cnv);
+        render();    
+    }  
+    
+    //Fucnção para Renderizar
+    function render(){
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        for(i=0; i < sprites.length; i++){
+            var spr = sprites[i];
+            if (spr.visible){
+                ctx.fillStyle = spr.color;
+                ctx.fillRect(spr.posX, spr.posY, spr.width, spr.height);                
+            }
+        }
+    } 
+    loop();
+}());
