1f0fbc01c023ce74a44e94cd4f731f8b204ee561
João Vitor Lonardoni <134065160+Joaovltgamer929@users.noreply.github.com>
2024-06-26 08:13:26 -0300

    Javas Scripts

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function(){
+    //Variaveis
+    var cnv = document.querySelector("canvas");
+    var ctx = cnv.getContext("2d")
+    //Teclas
+    var LEFT = 37;
+    var UP = 38;
+    var RIGTH = 39;
+    var DOWN = 40;
+    //Movimentos
+
+}
